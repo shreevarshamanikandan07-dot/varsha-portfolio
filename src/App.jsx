df3884fd4525403b932fb93114ac855c404f15c3
shreevarshamanikandan07-dot <shreevarshamanikandan.07@gmail.com>
2026-05-22 21:58:08 +0530

    Removed animated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,13 @@ import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
 import Footer from "./components/Footer";
-import AnimatedCursor from "react-animated-cursor";
+
 
 function App() {
   return (
     
     <div className="relative overflow-hidden">
-      <AnimatedCursor
-  innerSize={8}
-  outerSize={35}
-  color="56, 189, 248"
-  outerAlpha={0.2}
-  innerScale={0.7}
-  outerScale={2}
-  clickables={[
-    "a",
-    "button",
-    ".link"
-  ]}
-/>
+      
 
       {/* Background Glow */}
       <div className="absolute top-[-200px] left-[-100px] w-[500px] h-[500px] bg-sky-500 opacity-20 blur-3xl rounded-full"></div>
